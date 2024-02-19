1281f74bc419be263db4c598a6d9d3b889273e99
shalinrabey <leonardomigguiel@gmail.com>
2024-02-18 21:50:16 -0300

    Created task component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TaskComponent from './components/Task'
+
+function App() {
+
+  return (
+    <>
+    <TaskComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Task.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Task.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
+function TaskComponent() {
+  return (
+    <div className="task">
+        <div className="task-status">
+            <div className="status-value">
+                <p className="status">Done</p>
+            </div>
+            <div className="status-line"></div>
+        </div>
+        <div className="identifier">    
+            <div className="task-title">
+                <p className="title">Task Title</p>
+            </div>
+            <div className="task-description">
+                <p className="description">Task Description</p>
+            </div>
+        </div>
+        <div className="task-time">
+            <div className="task-icon">
+            <FontAwesomeIcon icon={faClock} />
+            </div>
+            <div className="time">5:00 PM</div>
+        </div>
+    </div>
+  )
+}
+
+export default TaskComponent
