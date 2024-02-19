ae0a8c39278ceed3dc13c32b386db15eca419bd3
peregozo <160425803+peregozo@users.noreply.github.com>
2024-02-19 17:52:34 -0300

    To-do-list api

--- to-do-list-api/Todo-List-Project/src/main/java/todolist/todolistproject/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package todolist.todolistproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import todolist.todolistproject.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-    
-}
